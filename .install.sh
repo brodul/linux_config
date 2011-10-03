63f888f443a84fc325d99e1efaf643aa0e0a5989
Andraz Brodnik <brodul@gmx.com>
2011-10-03 19:33:56 +0200

    rewrite

--- .install.sh
+++ .install.sh
@@ -2,13 +2,9 @@
 
 cd /tmp/
 
-if [ -d "linux_config" ]; then
-  rm -rf  linux_config
-fi
 
-git clone git://github.com/brodul/linux_config.git
+git clone git://github.com/brodul/linux_config.git || git fetch; git rebase origin master
 
 
-rm -rf linux_config/.git
-cp -r ./.[a-zA-Z0-9]*  $HOME/
+rsync -av .bazinga/*  $HOME/
 
